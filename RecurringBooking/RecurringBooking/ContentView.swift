e3e016e44dc3f2ec2026402906fc5b58d3a34ba1
patilvirendra0 <virendra.patil@xplortechnologies.com>
2023-12-08 13:11:26 +0530

    initial commit
    Project Started

---
+++ RecurringBooking/RecurringBooking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecurringBooking
+//
+//  Created by Virendra Patil on 08/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecurringBooking/RecurringBooking/RecurringBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecurringBookingApp.swift
+//  RecurringBooking
+//
+//  Created by Virendra Patil on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RecurringBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
